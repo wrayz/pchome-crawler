9f6bae85a998365aae52f8a253b4ec85dc893dec
WRayz <ruizhouwu@gmail.com>
2019-04-12 09:36:44 +0800

    feat: 爬取查詢商品關鍵字結果
    查詢關鍵字還是 hard code

--- custom_crawler/pchome.py
+++ custom_crawler/pchome.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 
+import time
 import requests
 import urllib3
 urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL'
@@ -7,10 +8,16 @@ urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL'
 
 class PChome:
     def __init__(self, query):
-        self._url = 'https://www.pcstore.com.tw/'
-        self._query = query
+        self._url = 'https://www.pcstore.com.tw/adm/psearch.htm'
+        self._url += '?store_k_word=JUU2JUIwJUEzJUU1JUJBJUE3&slt_k_option=1'
+        self._data = {
+            'ltg_p': ' ',
+            'after_login': ' ',
+            'slt_k_option': 1,
+            'store_k_word': '%AE%F0%AEy'
+        }
 
     def get_page(self):
-        r = requests.get(self._url)
+        r = requests.post(self._url, data=self._data)
         r.encoding = 'big5'
-        return BeautifulSoup(r.text, 'html.parser')
+        return BeautifulSoup(r.content, 'html.parser')
--- main.py
+++
@@ -1,6 +0,0 @@
-from custom_crawler.pchome import PChome
-
-pchome = PChome('xxx')
-page = pchome.get_page()
-
-print(page.prettify())
